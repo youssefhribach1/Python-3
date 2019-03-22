7ffff34adb1ffb996cd3da75adff653d5238dce3
youssefhribach1@gmail.com <java0109>
2019-03-22 13:00:21 +0100

    Initial commit

---
+++ format_method.py
@@ -0,0 +1,24 @@
+#strings
+print("today i had {0} cups of {1}".format(3,"coffee"))
+
+print('prices:({x}, {y}, {z})'.format(x=2,y=10,z=2.5))
+
+print("the {vehicle} had {0} crashes in {1} months".format(2,6,vehicle='car'))
+
+
+print('{:<20}'.format("python"))
+print('{:>40}'.format("python"))
+
+print('{:b}'.format(25))
+print('{:x}'.format(25))
+print('{:o}'.format(25))
+
+
+print('i\'am youssef')
+
+print("""\ 
+           hello:
+                
+                 dear youssef hribach
+                
+                i\'am waiting your reply  """)
